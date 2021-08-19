c2d7e92ce6bc586d74183a78bb21143e076f403f
younger-k <younger9403@gmail.com>
2021-08-19 23:49:16 +0900

    feat: keyEvnet 적용 - 화살표키로 위치 이동

--- src/components/IdolQuestion/IdolQuestion.style.tsx
+++ src/components/IdolQuestion/IdolQuestion.style.tsx
@@ -5,4 +5,21 @@ export const baseCss = css`
   height: auto;
   margin: auto;
   padding: 30px 0;
+  .grid-wrapper {
+    width: 300px;
+    display:flex;
+    border: 1px solid #333;
+    flex-direction: column;
+    .grid-row {
+      display:flex;
+      .grid-elem {
+        width: 30px;
+        height: 30px;
+        border: 1px solid #333;
+        &.color {
+          background-color: #333;
+        }
+      }
+    }
+  }
 `
--- src/components/IdolQuestion/IdolQuestion.tsx
+++ src/components/IdolQuestion/IdolQuestion.tsx
@@ -14,29 +14,66 @@ const defaultAnswer = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
 interface Position {
-  x: number;
-  y: number;
+  row: number;
+  col: number;
 }
 
 export const IdolQuestion = (): ReactElement => {
-  const [answer, setAnswer] = useState<number[][]>(defaultAnswer);
-  const [position, setPosition] = useState<Position>({x: 10, y: 0})
+  const [grid, setGrid] = useState<number[][]>(defaultAnswer);
+  const [position, setPosition] = useState<Position>({row: 9, col: 0});
 
   const handleArrowKey = (event: KeyboardEvent) => {
-    console.log(event.key);
+    const nextPosition: Position = {...position};
+    switch (event.key) {
+      case 'ArrowUp':
+        if (position.row > 0) nextPosition.row = position.row - 1;
+        setPosition(nextPosition);
+        break;
+      case 'ArrowDown':
+        if (position.row < 9) nextPosition.row = position.row + 1;
+        setPosition(nextPosition);
+        break;
+      case 'ArrowRight':
+        if (position.col < 9)  nextPosition.col = position.col + 1;
+        setPosition(nextPosition);
+        break;
+      case 'ArrowLeft':
+        if (position.col > 0)  nextPosition.col = position.col - 1;
+        setPosition(nextPosition);
+        break;
+      default:
+        break;
+    }
   }
 
   useEffect(() => {
-    window.addEventListener("keydown", handleArrowKey)
-  }, [])
+    window.addEventListener("keydown", handleArrowKey);
+    return () => window.removeEventListener("keydown", handleArrowKey);
+  }, [position])
 
   return (
     <div className="question-wrapper" css={baseCss}>
-
+      <div className="grid-wrapper">
+        {grid.map((column: number[], rowIdx) => {
+          return (
+            <div className="grid-row">
+              {column.map((elem: number, colIdx) => {
+                return (
+                  <div className={`grid-elem ${grid[rowIdx][colIdx] > 0 ? 'color' : ''}`}>
+                    {position.row === rowIdx && position.col === colIdx && (
+                      <div>POS</div>
+                    )}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
