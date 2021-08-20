49f0354de843144f743048e542d48cdc7c192322
younger-k <younger9403@gmail.com>
2021-08-20 23:56:30 +0900

    feat: 문제 정답 확인기능

--- src/components/IdolQuestion/IdolQuestion.style.tsx
+++ src/components/IdolQuestion/IdolQuestion.style.tsx
@@ -5,21 +5,65 @@ export const baseCss = css`
   height: auto;
   margin: auto;
   padding: 30px 0;
-  .grid-wrapper {
-    width: 300px;
-    display:flex;
-    border: 1px solid #333;
-    flex-direction: column;
-    .grid-row {
+  .question-description {
+    margin-bottom: 50px;
+    .note {
+      padding: 10px;
+      line-height: 1.5;
+      border: 1px solid #555;
+      color: #555;
+      width: 340px;
+      margin-bottom: 12px;
+    }
+  }
+  .question-explain {
+    font-size: 14px;
+    color: #555;
+    margin-bottom: 8px;
+  }
+  .answer-form-wrapper {
+    display: flex;
+    .grid-wrapper {
+      width: 300px;
       display:flex;
-      .grid-elem {
-        width: 30px;
-        height: 30px;
-        border: 1px solid #333;
-        &.color {
-          background-color: #333;
+      border: 1px solid #333;
+      flex-direction: column;
+      .grid-row {
+        display:flex;
+        .grid-elem {
+          display: flex;
+          width: 30px;
+          height: 30px;
+          border: 1px solid #333;
+          justify-content: center;
+          &.color {
+            background-color: #aaa;
+          }
+          .pos {
+            width: 20px;
+            height: 20px;
+            border-radius: 15px;
+            margin: auto 0;
+            background-color: #cc0033;
+          }
         }
       }
+    }
+    .image-wrapper {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      margin-right: 20px;
+      img {
+        width: 80px;
+        height: 80px;
+      }
+    }
+    .button-wrapper {
+      display:flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      margin-left: 20px;
     }
   }
 `
